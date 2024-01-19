47609f350a3477d5d4041d5ce619b58d911b5986
vkhydras <victorkimaru8@gmail.com>
2024-01-19 13:11:29 +0300

    zod

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -5,17 +5,18 @@ import Link from "next/link";
 import { useState } from "react"
 import { z } from "zod";
 import validator from "validator";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 
 const FormSchema = z.object({
     firstName: z.string()
         .min(2,"First Name ust be atleats two characters")
         .max(45,"Fist name must be less than 45 characters")
-        .regex(new RegExp("[a-zA-A]+$","No special characters allow")),
+        .regex(new RegExp("[a-zA-A]+$"),"No special characters allow"),
     lastName: z.string()
         .min(2,"First Name ust be atleats two characters")
         .max(45,"Fist name must be less than 45 characters")
-        .regex(new RegExp("[a-zA-A]+$","No special characters allow")),
+        .regex(new RegExp("[a-zA-A]+$"),"No special characters allow"),
     email: z.string()
         .email("Please enter a valid email"),
     phone: z.string()
@@ -23,7 +24,7 @@ const FormSchema = z.object({
     password: z.string()
         .min(6,"Password must be atleast 6 characters")
         .max(50,"Password must be less than 50 characters"),
-    comfirmPassword: z.string()
+    confirmPassword: z.string()
         .min(6,"Password must be atleast 6 characters")
         .max(50,"Password must be less than 50 characters"),
     accepted: z.literal(true,{
@@ -32,34 +33,47 @@ const FormSchema = z.object({
         })
     })
 
-}).refine(data=> data.password===data.comfirmPassword,{
+}).refine(data=> data.password===data.confirmPassword,{
     message:"Password and comfirm password do not match",
     path:["password","confirmPassword"],
 })
 
+type InputType = z.infer<typeof FormSchema>
+
 const SignupForm = () => {
+
+    const {register,handleSubmit,reset,control} = useForm<InputType>()
 
     const [visible,setVisible] = useState(false)
     const toggleVisible = () => setVisible(prev=>!prev)
 
-    return <form className="grid grid-cols-2 gap-3 p-2 shadow border rounded-md place-self-stretch"  action="">
-        <Input label="first Name" startContent={<UserIcon className="w-4"/>}></Input>
+    const saveUser: SubmitHandler<InputType> = async(data) =>{
+        console.log(data)
+    }
 
-        <Input label="Last Name" startContent={<UserIcon className="w-4"/>}></Input>
+    return <form onSubmit={handleSubmit(saveUser)} className="grid grid-cols-2 gap-3 p-2 shadow border rounded-md place-self-stretch"  action="">
+        <Input {...register("firstName")}label="first Name" startContent={<UserIcon className="w-4"/>}></Input>
 
-        <Input className="col-span-2" label="Email" startContent={<EnvelopeIcon className="w-4"/>}></Input>
+        <Input {...register("lastName")} label="Last Name" startContent={<UserIcon className="w-4"/>}></Input>
 
-        <Input className="col-span-2" label="Phone" startContent={<PhoneIcon className="w-4"/>}></Input>
+        <Input {...register("email")} className="col-span-2" label="Email" startContent={<EnvelopeIcon className="w-4"/>}></Input>
 
-        <Input className="col-span-2" label="Password" type={visible? "text":"password"} startContent={<KeyIcon className="w-4"/>} endContent={
+        <Input {...register("phone")} className="col-span-2" label="Phone" startContent={<PhoneIcon className="w-4"/>}></Input>
+
+        <Input {...register("password")} className="col-span-2" label="Password" type={visible? "text":"password"} startContent={<KeyIcon className="w-4"/>} endContent={
         visible? <EyeSlashIcon className="w-4 cursor-pointer" onClick={toggleVisible}/>
             :
          <EyeIcon className="w-4 cursor-pointer" onClick={toggleVisible}/>
         }></Input>
 
-        <Input className="col-span-2" label="Comfirm Password" type={visible? "text":"password"} startContent={<KeyIcon className="w-4"/>}></Input>
+        <Input {...register("confirmPassword")} className="col-span-2" label="Comfirm Password" type={visible? "text":"password"} startContent={<KeyIcon className="w-4"/>}></Input>
 
-        <Checkbox className="col-span-2">I accept The <Link href="/Terms">Terms</Link></Checkbox>
+        <Controller control={control} name="accepted" render={({field})=>(
+            <Checkbox onChange={field.onChange} onBlur={field.onBlur} className="col-span-2">
+                I accept The <Link href="/Terms">Terms</Link>
+            </Checkbox>
+        )} />
+
 
         <div className="flex justify-center col-span-2">
             <Button type="submit" color="primary" className="w-48">Submit</Button>
