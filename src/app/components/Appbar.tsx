0d8bd6d2e1edf34f932db9fbb7ca9b82c68378f6
vkhydras <victorkimaru8@gmail.com>
2024-01-20 10:19:51 +0300

    signInFunc

--- src/app/components/Appbar.tsx
+++ src/app/components/Appbar.tsx
@@ -1,5 +1,6 @@
 import { Button, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
 import Link from "next/link";
+import SignInButton from "./SignInButton";
 
 const Appbar = () => {
   return (
@@ -17,7 +18,7 @@ const Appbar = () => {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} color="primary" href="/auth/signup" variant="flat">Sign Up</Button>
+          <SignInButton />
         </NavbarItem>
       </NavbarContent>
     </Navbar>
---
+++ src/app/components/SignInButton.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Button } from '@nextui-org/react'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function SignInButton() {
+
+    const {data:session} = useSession()
+
+  return (
+    <div className='flex items-center gap-2'>
+      {session && session.user?
+    <>
+       <p>{session.user.email}</p>
+       <Link href="/api/auth/signout" className='text-sky-500 hover:text-sky-600 transition-colors'>Sign Out</Link>
+    </>:
+    <>
+        <Button as={Link} href='/api/auth/signin'>Sign In</Button>
+        <Button as={Link} href='/auth/signup'>Sign Up</Button>
+    </>
+    }
+    </div>
+  )
+}
